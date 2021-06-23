7392169172803e61160ca09517b06f18f3ecdeb8
yashmandhaniya <85881993+yashmandhaniya@users.noreply.github.com>
2021-06-24 01:29:52 +0530

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,11 @@
+marks = int(input())
+if (marks<100 and marks>=90):
+    print("A division")
+    elif(marks<90 and marks>=80):
+        print("B division")
+        elif(marks<80 and marks>=60):
+            print("C division")
+            elif(marks<60 and marks>=40):
+                print("D division")
+                else:
+                    print("fail")
